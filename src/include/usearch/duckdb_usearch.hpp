7cabc1f8cb1b92d1e579e6ca033521b049e48b9f
Shravan Sunder <ShravanSunder@users.noreply.github.com>
2025-05-03 19:28:01 -0400

    stubs for usearch

--- src/include/usearch/duckdb_usearch.hpp
+++ src/include/usearch/duckdb_usearch.hpp
@@ -1,16 +1,9 @@
 #pragma once
 
-#include <unordered_map>
-#include <cfloat>
+// Include the main Usearch stubs
+#include "index.hpp"
+#include "index_dense.hpp"
 
-#define USEARCH_USE_SIMSIMD DUCKDB_USEARCH_USE_SIMSIMD
-#define USEARCH_USE_FP16LIB 1
-#define USEARCH_USE_OPENMP  0
-
-#include "usearch/index.hpp"
-#include "usearch/index_dense.hpp"
-#include "usearch/index_plugins.hpp"
-
-#undef USEARCH_USE_SIMSIMD
-#undef USEARCH_USE_FP16LIB
-#undef USEARCH_USE_OPENMP
+// No need to include index_plugins.hpp as its functionality (metrics, executors, etc.)
+// should be stubbed out within index.hpp and index_dense.hpp or isn't called by HNSW.
+// No need for the preprocessor defines like USEARCH_USE_SIMSIMD in the stub. 
